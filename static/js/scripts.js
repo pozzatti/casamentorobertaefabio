5caa6b9544af48fe4cb622294b1973cbf786fdbb
pozzatti <f.pozzatti@gmail.com>
2023-09-19 12:12:16 -0300

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -872,6 +872,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 const xhr = new XMLHttpRequest();
                 xhr.open('POST', '/confirmacoes/?format=json', true);
                 xhr.setRequestHeader('Content-Type', 'application/json');
+                xhr.setRequestHeader('X-CSRFToken', formData.csrfmiddlewaretoken.value);
         
                 xhr.onreadystatechange = function () {
                     if (xhr.readyState === XMLHttpRequest.DONE) {
